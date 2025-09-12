1f138a58add5ad4b2f247808dc7138b7bfa0f43f
highonhues <ananya1.sci@gmail.com>
2025-09-12 03:00:32 -0700

    Running cellranger multi to demultiplex the COUPKO samples

--- scripts/multi_cell/cellranger_multi.sh
+++ scripts/multi_cell/cellranger_multi.sh
@@ -4,16 +4,18 @@
 #SBATCH --cpus-per-task=28
 #SBATCH --time=72:00:00
 #SBATCH --partition=nodes                     
-#SBATCH --output=/scratch/home/agupta1/coup/logs/cRm_%A_%j.output
-#SBATCH --error=/scratch/home/agupta1/coup/logs/cRm_%A_%j.error
+#SBATCH --output=/scratch/home/agupta1/coup/logs/cRM_%j.output
+#SBATCH --error=/scratch/home/agupta1/coup/logs/cRM_%j.error
 
 source /home/agupta1/miniconda3/etc/profile.d/conda.sh
 conda activate cellranger
 
-outdir=/scratch/home/agupta1/coup/results/cr_multi
+outdir=/scratch/home/agupta1/coup/results/cr_multi_coupko
 mkdir -p "$outdir"
 
 
 # https://www.biostars.org/p/9587394/ naming convention
 
-cellranger multi --id=PP2_RBPJKO_multi --csv=/scratch/home/agupta1/coup/scripts/multi_cell/multiconfig.csv --localcores=$SLURM_CPUS_PER_TASK --localmem=$(( SLURM_MEM_PER_NODE / 1024 )) --output-dir="${outdir}"
+cellranger multi --id=COUPKO_multi --csv=/scratch/home/agupta1/coup/scripts/multi_cell/multiconfig_coupko.csv --localcores=$SLURM_CPUS_PER_TASK --localmem=$(( SLURM_MEM_PER_NODE / 1024 )) --output-dir="${outdir}"
+
+# cellranger multi --id=PP2_RBPJKO_multi --csv=/scratch/home/agupta1/coup/scripts/multi_cell/multiconfig.csv --localcores=$SLURM_CPUS_PER_TASK --localmem=$(( SLURM_MEM_PER_NODE / 1024 )) --output-dir="${outdir}"
